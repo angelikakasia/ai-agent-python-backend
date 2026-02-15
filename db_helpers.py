e90811695ee98da6a656621d026aee6e8f187bb4
suzanaai <angelska327@gmail.com>
2026-02-15 11:22:26 -0600

    ready for heroku deploy

--- db_helpers.py
+++ db_helpers.py
@@ -1,17 +1,36 @@
+# import os
+# import psycopg2
+
+# def get_db_connection():
+#     if 'ON_HEROKU' in os.environ:
+#         connection = psycopg2.connect(
+#             os.getenv('DATABASE_URL'),
+#             sslmode='require'
+#         )
+#     else:
+#         connection = psycopg2.connect(
+#             host='localhost',
+#             database=os.getenv('POSTGRES_DATABASE'),
+#             user=os.getenv('POSTGRES_USERNAME'),
+#             password=os.getenv('POSTGRES_PASSWORD')
+#         )
+#     return connection
 import os
 import psycopg2
 
+
 def get_db_connection():
-    if 'ON_HEROKU' in os.environ:
-        connection = psycopg2.connect(
-            os.getenv('DATABASE_URL'),
-            sslmode='require'
+    # Heroku production database
+    if os.getenv("ON_HEROKU") == "true":
+        return psycopg2.connect(
+            os.getenv("DATABASE_URL"),
+            sslmode="require"
         )
-    else:
-        connection = psycopg2.connect(
-            host='localhost',
-            database=os.getenv('POSTGRES_DATABASE'),
-            user=os.getenv('POSTGRES_USERNAME'),
-            password=os.getenv('POSTGRES_PASSWORD')
-        )
-    return connection
+
+    # Local development database
+    return psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        database=os.getenv("POSTGRES_DATABASE"),
+        user=os.getenv("POSTGRES_USERNAME"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
